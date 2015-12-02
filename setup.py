e79da2e4ae91ff51c2ff642a70da9808827d72b3
Karl-Philipp Richter <krichter722@aol.de>
2015-12-03 00:12:29 +0100

    changed versioning from static to version_command in setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,6 @@ from pkg_resources import parse_version
 
 setup(
     name = "template-helper",
-    version = "1.0",
+    version_command=("git describe --tags", "pep440-git-local"),
     packages = ["."],
 )
